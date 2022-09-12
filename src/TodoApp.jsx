261c62615e1291adb66fdf7aa1ae2f421634d9c6
Angel Travieso <angeltraviesoc@gmail.com>
2022-09-12 13:49:29 -0400

    consumir API mediante el custom hook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,14 +1,33 @@
+import { useGetTodosQuery } from "./store/apis"
+
 export const TodoApp = () => {
+
+  // Cambiar nombre a la propiedad destructurada (data: [nuevo_nombre])
+  const { data: todos = [], isLoading } = useGetTodosQuery();
+
+
+
   return (
     <>
         <h1>Todos - RTK Query</h1>
         <hr />
 
-        <h4>IsLoading...</h4>
+        <h4>IsLoading: { isLoading ? 'True' : 'False' } </h4>
 
         <pre>
-            ...
+            
         </pre>
+
+        <ul>
+          {
+            todos.map( todo => (
+              <li key={ todo.id }>
+                <strong>{ todo.completed ? 'DONE' : 'Pending' } </strong>
+                { todo.title }
+              </li>
+            ))
+          }
+        </ul>
 
         <button>
             Next Todo
